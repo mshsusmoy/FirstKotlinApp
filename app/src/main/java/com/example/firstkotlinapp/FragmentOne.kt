639176ea1102a9f14065cb0e3966c36c15d198a6
mshsusmoy <mshsusmoy@gmail.com>
2019-07-28 22:44:33 +0600

    Fragment Implemented

---
+++ app/src/main/java/com/example/firstkotlinapp/FragmentOne.kt
@@ -0,0 +1,51 @@
+package com.example.firstkotlinapp
+
+import android.app.Activity
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class FragmentOne: Fragment() {
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_one,container,false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+    }
+}
--- app/src/main/java/com/example/firstkotlinapp/MainActivity.kt
+++ app/src/main/java/com/example/firstkotlinapp/MainActivity.kt
@@ -8,14 +8,21 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    var isFragmentOne = true
+    var transaction = supportFragmentManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        showFragmentOne()
+
 
         hello_button.setOnClickListener {
             textview_say.setText("Hi Susmoy!")
             hello_button.setBackgroundColor(Color.WHITE)
             hello_button.setText("Hi")
+            if(isFragmentOne) showFragmentTwo() else showFragmentOne()
         }
 
         val numberDataArray: ArrayList<String> = ArrayList()
@@ -25,5 +32,23 @@ class MainActivity : AppCompatActivity() {
 
         recyclerview.layoutManager = LinearLayoutManager(this)
         recyclerview.adapter = DemoAdapter(numberDataArray)
+    }
+
+    fun showFragmentOne(){
+        val fragment_transaction_one = transaction.beginTransaction()
+        val fragmentOne = FragmentOne()
+        fragment_transaction_one.replace(R.id.fragment_container,fragmentOne)
+        fragment_transaction_one.addToBackStack(null)
+        fragment_transaction_one.commit()
+        isFragmentOne = true
+    }
+
+    fun showFragmentTwo(){
+        val fragment_transaction_two = transaction.beginTransaction()
+        val fragmentOne = FragmentTwo()
+        fragment_transaction_two.replace(R.id.fragment_container,fragmentOne)
+        fragment_transaction_two.addToBackStack(null)
+        fragment_transaction_two.commit()
+        isFragmentOne = false
     }
 }
